f992c084554bc739f6d6084a0ac541184dc4367c
diego.molina <diego.molina@itac.co>
2021-02-08 20:19:43 -0500

    Componente buscador

--- src/app/buscador/buscador.module.ts
+++ src/app/buscador/buscador.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BuscadorComponent } from './buscador/buscador.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [BuscadorComponent],
   imports: [
     CommonModule
   ]
