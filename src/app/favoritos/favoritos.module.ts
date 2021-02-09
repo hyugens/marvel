b3b5e9621ec96cb5d0d140aa3539498383580f83
diego.molina <diego.molina@itac.co>
2021-02-08 20:20:03 -0500

    Componente favoritos

--- src/app/favoritos/favoritos.module.ts
+++ src/app/favoritos/favoritos.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FavoritosComponent } from './favoritos/favoritos.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [FavoritosComponent],
   imports: [
     CommonModule
   ]
