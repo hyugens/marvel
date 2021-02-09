069bae47e91cbdd30cd088b87fbde17002c82099
diego.molina <diego.molina@itac.co>
2021-02-08 20:18:26 -0500

    Componente detalle

--- src/app/detalle/detalle.module.ts
+++ src/app/detalle/detalle.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DetalleComponent } from './detalle/detalle.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [DetalleComponent],
   imports: [
     CommonModule
   ]
