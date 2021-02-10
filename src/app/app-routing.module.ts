a5a304904eb9c178f8e2b54ced1c8e8782a1c487
diego.molina <diego.molina@itac.co>
2021-02-09 21:34:11 -0500

    Adelanto Buscador header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [
   { path: 'buscador', loadChildren: () => import('./buscador/buscador.module').then(m => m.BuscadorModule) },
   { path: 'detalle', loadChildren: () => import('./detalle/detalle.module').then(m => m.DetalleModule) },
-  { path: 'favoritos', loadChildren: () => import('./favoritos/favoritos.module').then(m => m.FavoritosModule) }
 ];
 
 @NgModule({
--- src/app/favoritos/favoritos.module.ts
+++ src/app/favoritos/favoritos.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FavoritosComponent } from './favoritos/favoritos.component';
-import { FavoritosRoutingModule } from './favoritos-routing.module';
-
-
 
 @NgModule({
   declarations: [FavoritosComponent],
   imports: [
-    CommonModule,
-    FavoritosRoutingModule
+    CommonModule
   ]
 })
 export class FavoritosModule { }
