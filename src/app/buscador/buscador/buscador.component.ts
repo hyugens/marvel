3961f0fc46238cba3b668354789eb3e1a16fa9bb
diego.molina <diego.molina@itac.co>
2021-02-11 15:12:15 -0500

    Termina funcionalidad Paginator

--- src/app/buscador/buscador/buscador.component.ts
+++ src/app/buscador/buscador/buscador.component.ts
@@ -3,6 +3,7 @@ import {MarvelApiService} from '../../services/marvel-api.service';
 import {Observable} from 'rxjs';
 import {Data} from '../../interfaces/character';
 import {Router} from '@angular/router';
+import {tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-buscador',
@@ -11,12 +12,19 @@ import {Router} from '@angular/router';
 })
 export class BuscadorComponent implements OnInit {
   characters$: Observable<Data>
+  maxLength = 1;
+  limitLength: number;
 
   constructor(private marvelApi: MarvelApiService,
               private router: Router) { }
 
   ngOnInit(): void {
-    this.characters$ = this.marvelApi.getCharacters();
+    this.characters$ = this.marvelApi.getCharacters().pipe(
+      tap((characters) => {
+        this.maxLength = characters.total;
+        this.limitLength = characters.limit;
+      })
+    );
   }
 
   detail(idHero: number) {
--- src/app/reusable/paginator/paginator.component.ts
+++ src/app/reusable/paginator/paginator.component.ts
@@ -1,5 +1,10 @@
 import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angular/core';
 
+export class PaginationButton {
+  tag: string;
+  id: string;
+}
+
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
@@ -7,10 +12,16 @@ import {Component, EventEmitter, Input, OnChanges, OnInit, Output} from '@angula
 })
 export class PaginatorComponent implements OnInit, OnChanges {
 
+
   // @Input() arrayLength: number;
   @Input() limitLength: number;
   @Input() maxResults: number;
   @Output() nextPage = new EventEmitter<number>();
+  actualPage = 1;
+  rangeButtons = 5;
+  maxButtonPage =  1;
+  minButtonPage = 1;
+  totalPages = 1;
   pages = [];
   constructor() { }
 
@@ -24,12 +35,98 @@ export class PaginatorComponent implements OnInit, OnChanges {
   }
 
   setPagination() {
-    const pages = Math.ceil(this.maxResults / this.limitLength);
-    for(let i = 1; i <= pages; i++) this.pages.push(i);
+    this.totalPages = Math.ceil(this.maxResults / this.limitLength);
+    this.viewInitialButtons();
   }
 
   navigateTo(page) {
     console.log('navigateTO: ', page);
-    this.nextPage.emit(page);
+    this.actualPage = page;
+    console.log('ActualPage: ', this.actualPage);
+    // this.nextPage.emit(page);
   }
+
+  next() {
+    this.actualPage++;
+    console.log('ActualPage: ', this.actualPage);
+
+  }
+
+  previous() {
+    this.actualPage--;
+    console.log('ActualPage: ', this.actualPage);
+  }
+
+  more() {
+    this.pages=[];
+    const auxMax = this.maxButtonPage + this.rangeButtons;
+    this.minButtonPage = this.maxButtonPage + 1;
+    this.actualPage = this.minButtonPage;
+
+    const pageButton = new PaginationButton();
+    pageButton.id = 'less'; pageButton.tag = '...';
+    this.pages.push(pageButton);
+
+    for(let i = this.maxButtonPage +1; i <= this.totalPages && i <= auxMax; i++){
+      const pageButton = new PaginationButton();
+      pageButton.id = i + ''; pageButton.tag = i + '';
+      this.pages.push(pageButton);
+    }
+
+    if (auxMax < this.totalPages) {
+      this.maxButtonPage = auxMax;
+      const pageButton = new PaginationButton();
+      pageButton.id = 'more'; pageButton.tag = '...';
+      this.pages.push(pageButton);
+    } else this.maxButtonPage = this.totalPages;
+
+  }
+
+  less() {
+    this.pages=[];
+    const auxMin = this.minButtonPage - this.rangeButtons;
+    this.maxButtonPage = this.minButtonPage -1;
+    this.actualPage = this.maxButtonPage;
+
+    for( let i = this.minButtonPage -1; i >= 1 && i >= auxMin; i--) {
+      const pageButton = new PaginationButton();
+      pageButton.id = i + ''; pageButton.tag = i + '';
+      this.pages.unshift(pageButton);
+    }
+
+    if (auxMin > 1) {
+      this.minButtonPage = auxMin;
+      const pageButton = new PaginationButton();
+      pageButton.id = 'less'; pageButton.tag = '...';
+      this.pages.unshift(pageButton);
+    } else this.minButtonPage = 1;
+
+    const pageButton = new PaginationButton();
+    pageButton.id = 'more'; pageButton.tag = '...';
+    this.pages.push(pageButton);
+
+  }
+
+  viewInitialButtons() {
+    this.pages = [];
+    if(this.totalPages <= this.rangeButtons) {
+      this.maxButtonPage = this.totalPages;
+      for(let i = 1; i <= this.totalPages; i++) {
+        const pageButton = new PaginationButton();
+        pageButton.id = i + ''; pageButton.tag = i + '';
+        this.pages.push(pageButton);
+      }
+    } else {
+      this.maxButtonPage = this.rangeButtons;
+      for(let i = 1; i <= this.rangeButtons; i++) {
+        const pageButton = new PaginationButton();
+        pageButton.id = i + ''; pageButton.tag = i + '';
+        this.pages.push(pageButton);
+      }
+      const pageButton = new PaginationButton();
+      pageButton.id = 'more'; pageButton.tag = '...';
+      this.pages.push(pageButton);
+    }
+  }
+
 }
