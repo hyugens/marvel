60bf31f9bba0b00a245eeafd92994e4d1397f1ec
diego.molina <diego.molina@itac.co>
2021-02-11 19:39:40 -0500

    Utiliza pipe Truncate en componente card

--- src/app/reusable/reusable.module.ts
+++ src/app/reusable/reusable.module.ts
@@ -4,12 +4,14 @@ import { CardComponent } from './card/card.component';
 import {MatDialogModule} from '@angular/material/dialog';
 import { ModalComponent } from './modal/modal.component';
 import { PaginatorComponent } from './paginator/paginator.component';
+import {PipesModule} from '../pipes/pipes.module';
 
 
 @NgModule({
   declarations: [CardComponent, ModalComponent, PaginatorComponent],
   imports: [
     CommonModule,
+    PipesModule,
     MatDialogModule
   ],
   exports: [CardComponent, ModalComponent, PaginatorComponent]
