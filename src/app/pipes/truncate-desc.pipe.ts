f9a9437683f4b6dbaf88a86649dbad648a71640f
diego.molina <diego.molina@itac.co>
2021-02-12 06:01:57 -0500

    validaciones pipe truncate

--- src/app/pipes/truncate-desc.pipe.ts
+++ src/app/pipes/truncate-desc.pipe.ts
@@ -6,9 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TruncateDescPipe implements PipeTransform {
 
   transform(value: string, maxCharacters): string {
-    let aux = value.slice(0, maxCharacters);
-    if (value.length > maxCharacters) { aux = aux + ' ...'; }
-    if (value.length === 0) {
+    let aux = '';
+    if (value) {
+      aux = value.slice(0, maxCharacters);
+      if (value.length > maxCharacters) { aux = aux + ' ...'; }
+    }
+    if (value === null || value.length === 0) {
       aux = 'No description available';
     }
     return aux;
